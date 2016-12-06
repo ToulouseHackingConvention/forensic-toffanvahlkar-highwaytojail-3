791fa4571da271baac7db4c37d564139e691ebc6
Volodia Laniel <laniel.volodia@gmail.com>
2016-12-06 10:48:25 +0100

    Implement file encryption / decryption.

--- main.c
+++ main.c
@@ -1,13 +1,19 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
 #include <openssl/conf.h>
 #include <openssl/evp.h>
 #include <openssl/err.h>
-#include <string.h>
+
+#define SRC_PATH "./flag.png"
+#define DST_PATH "./flag.cipher"
 
 void handleErrors(void);
-int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key,
-        unsigned char *iv, unsigned char *ciphertext);
-int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
-        unsigned char *iv, unsigned char *plaintext);
+int encrypt(unsigned char *key, unsigned char *iv, int fsrc, int fdst);
+int decrypt(unsigned char *key, int fsrc, int fdst);
 
 int main (void)
 {
@@ -21,44 +27,62 @@ int main (void)
     /* A 128 bit IV */
     unsigned char *iv = (unsigned char *)"01234567890123456";
 
-    /* Message to be encrypted */
-    unsigned char *plaintext =
-        (unsigned char *)"The quick brown fox jumps over the lazy dog";
-
-    /* Buffer for ciphertext. Ensure the buffer is long enough for the
-     * ciphertext which may be longer than the plaintext, dependant on the
-     * algorithm and mode
-     */
-    unsigned char ciphertext[128];
-
-    /* Buffer for the decrypted text */
-    unsigned char decryptedtext[128];
-
-    int decryptedtext_len, ciphertext_len;
+    /* Files to be used. */
+    int fplain;
+    int fcipher;
+    int fdec;
 
     /* Initialise the library */
     ERR_load_crypto_strings();
     OpenSSL_add_all_algorithms();
     OPENSSL_config(NULL);
 
-    /* Encrypt the plaintext */
-    ciphertext_len = encrypt (plaintext, strlen ((char *)plaintext), key, iv,
-            ciphertext);
-
-    /* Do something useful with the ciphertext here */
-    printf("Ciphertext is:\n");
-    BIO_dump_fp (stdout, (const char *)ciphertext, ciphertext_len);
-
-    /* Decrypt the ciphertext */
-    decryptedtext_len = decrypt(ciphertext, ciphertext_len, key, iv,
-            decryptedtext);
-
-    /* Add a NULL terminator. We are expecting printable text */
-    decryptedtext[decryptedtext_len] = '\0';
-
-    /* Show the decrypted text */
-    printf("Decrypted text is:\n");
-    printf("%s\n", decryptedtext);
+    printf("Encryption...\n");
+    fplain = open("./flag.png", O_RDONLY);
+    if (fplain > -1)
+    {
+        fcipher = creat("./flag.cipher", S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+        if (fcipher > -1)
+        {
+            /* Encrypt the plaintext */
+            encrypt (key, iv, fplain, fcipher);
+            close(fcipher);
+        }
+        else
+        {
+            /* Handle errors */
+            printf("Error opening destination file!\n");
+        }
+        close(fplain);
+    }
+    else
+    {
+        /* Handle errors */
+        printf("Error opening source file!\n");
+    }
+    printf("Decryption...\n");
+    fcipher = open("./flag.cipher", O_RDONLY);
+    if (fcipher > -1)
+    {
+        fdec = creat("./flag.dec", S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+        if (fdec > -1)
+        {
+            /* Decrypt the ciphertext */
+            decrypt(key, fcipher, fdec);
+            close(fdec);
+        }
+        else
+        {
+            /* Handle errors */
+            printf("Error opening destination file!\n");
+        }
+        close(fcipher);
+    }
+    else
+    {
+        /* Handle errors */
+        printf("Error opening source file!\n");
+    }
 
     /* Clean up */
     EVP_cleanup();
@@ -73,14 +97,34 @@ void handleErrors(void)
     abort();
 }
 
-int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key,
-        unsigned char *iv, unsigned char *ciphertext)
+int encrypt(unsigned char *key, unsigned char *iv, int fsrc, int fdst)
 {
     EVP_CIPHER_CTX *ctx;
 
-    int len;
+    /* BIO_dump_fp (stdout, (const char *)ciphertext, ciphertext_len); */
 
-    int ciphertext_len;
+    /* Message to be encrypted */
+    unsigned char plaintext[128];
+    /* Length of the current plaintext buffer */
+    int plaintext_len = 0;
+
+    /* Buffer for ciphertext.
+     * Ensure the buffer is long enough for the ciphertext which may be longer than the plaintext, dependant on the algorithm and mode  */
+    unsigned char ciphertext[255];
+    /* Total length of the resulting ciphertext */
+    int ciphertext_len = 0;
+
+    /* Length variable used by cryptographic primitives */
+    int len = 0;
+
+    /* Writing the 128 bit IV at the beginning of the file */
+    len = write(fdst, iv, 16);
+    if (len != 16)
+    {
+        printf("Error writing IV ; len: %d\n", len);
+        exit(0);
+    }
+    ciphertext_len += len;
 
     /* Create and initialise the context */
     if(!(ctx = EVP_CIPHER_CTX_new())) handleErrors();
@@ -90,24 +134,34 @@ int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key,
      * In this example we are using 256 bit AES (i.e. a 256 bit key). The
      * IV size for *most* modes is the same as the block size. For AES this
      * is 128 bits */
-    printf("%s : %d : %d\n", plaintext, plaintext_len, plaintext_len * 8);
     if(1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
         handleErrors();
 
-    /* Provide the message to be encrypted, and obtain the encrypted output.
-     * EVP_EncryptUpdate can be called multiple times if necessary
-     */
-    if(1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len))
-        handleErrors();
-    ciphertext_len = len;
-    printf("First ciphertext_len : %d : %d\n", ciphertext_len, ciphertext_len * 8);
+    /* Get the beginning of the plaintext. */
+    plaintext_len = read(fsrc, plaintext, 128);
+    while (plaintext_len > 0)
+    {
+        /* Provide the message to be encrypted, and obtain the encrypted output.
+         * EVP_EncryptUpdate can be called multiple times if necessary
+         */
+        if(1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len)) handleErrors();
+        ciphertext_len += len;
+
+        /* Write the resulting ciphertext */
+        write(fdst, ciphertext, len);
+
+        /* Get the remaining of the plaintext */
+        plaintext_len = read(fsrc, plaintext, 128);
+    }
 
     /* Finalise the encryption. Further ciphertext bytes may be written at
      * this stage.
      */
-    if(1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len)) handleErrors();
+    if(1 != EVP_EncryptFinal_ex(ctx, ciphertext, &len)) handleErrors();
     ciphertext_len += len;
-    printf("Final ciphertext_len : %d : %d\n", ciphertext_len, ciphertext_len * 8);
+
+    /* Write the resulting ciphertext */
+    write(fdst, ciphertext, len);
 
     /* Clean up */
     EVP_CIPHER_CTX_free(ctx);
@@ -115,14 +169,28 @@ int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key,
     return ciphertext_len;
 }
 
-int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
-        unsigned char *iv, unsigned char *plaintext)
+int decrypt(unsigned char *key, int fsrc, int fdst)
 {
     EVP_CIPHER_CTX *ctx;
 
-    int len;
+    unsigned char iv[16];
 
-    int plaintext_len;
+    /* Buffer for ciphertext. */
+    unsigned char ciphertext[128];
+    int ciphertext_len = 0;
+
+    /* Buffer for the decrypted text */
+    unsigned char plaintext[255];
+    int plaintext_len = 0;
+
+    int len = 0;
+
+    len = read(fsrc, iv, 16);
+    if (len != 16)
+    {
+        printf("Error len: %d\n", len);
+        exit(0);
+    }
 
     /* Create and initialise the context */
     if(!(ctx = EVP_CIPHER_CTX_new())) handleErrors();
@@ -135,18 +203,34 @@ int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
     if(1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
         handleErrors();
 
-    /* Provide the message to be decrypted, and obtain the plaintext output.
-     * EVP_DecryptUpdate can be called multiple times if necessary
-     */
-    if(1 != EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len))
-        handleErrors();
-    plaintext_len = len;
+    /* Get the beginning of the ciphertext. */
+    ciphertext_len = read(fsrc, ciphertext, 128);
+    while(ciphertext_len > 0)
+    {
+        /* Provide the message to be decrypted, and obtain the plaintext output.
+         * EVP_DecryptUpdate can be called multiple times if necessary
+         */
+        if(1 != EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len))
+            handleErrors();
+        plaintext_len += len;
+
+        /* Write the resulting ciphertext */
+        write(fdst, plaintext, len);
+
+        /* Get the remaining of the plaintext */
+        ciphertext_len = read(fsrc, ciphertext, 128);
+    }
+    printf("len: %d\n", len);
 
     /* Finalise the decryption. Further plaintext bytes may be written at
      * this stage.
      */
-    if(1 != EVP_DecryptFinal_ex(ctx, plaintext + len, &len)) handleErrors();
+    if(1 != EVP_DecryptFinal_ex(ctx, plaintext, &len)) handleErrors();
+    printf("len: %d\n", len);
     plaintext_len += len;
+
+    /* Write the resulting plaintext */
+    write(fdst, plaintext, len);
 
     /* Clean up */
     EVP_CIPHER_CTX_free(ctx);
